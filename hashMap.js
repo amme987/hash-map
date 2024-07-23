7773db350ebb4bf38c31405ff81f8baa5d58d91d
amme987 <emmagination97@gmail.com>
2024-07-23 15:35:34 -0400

    Build set() function

--- hashMap.js
+++ hashMap.js
@@ -2,6 +2,12 @@ class HashMap {
   capacity;
   loadFactor;
 
+  constructor() {
+    // this.key = key;
+    // this.value = value;
+    this.buckets = Array.from({ length: 16 }, () => []);
+  }
+
   // takes a key and produces a hash code with it.
   hash(key) {
     let hashCode = 0;
@@ -11,10 +17,18 @@ class HashMap {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
 
-    return hashCode;
+    return hashCode % this.buckets.length;
   }
 
-  set(key, value) {}
+  set(key, value) {
+    let bucket = this.buckets[this.hash(key)];
+    let index = bucket.map(obj => obj.key).indexOf(key);
+    if (index !== -1) {
+      bucket[index].value = value;
+    } else {
+      bucket.push({ key, value });
+    }
+  }
 
   // takes one argument as a key and returns the value that is assigned to this key. If a key is not found, return null.
   get(key) {}
@@ -40,3 +54,22 @@ class HashMap {
   // returns an array that contains each key, value pair.
   entries() {}
 }
+
+const test = new HashMap();
+test.set('apple', 'red');
+test.set('banana', 'yellow');
+test.set('carrot', 'orange');
+test.set('dog', 'brown');
+test.set('elephant', 'gray');
+test.set('frog', 'green');
+test.set('grape', 'purple');
+test.set('hat', 'black');
+test.set('ice cream', 'white');
+test.set('jacket', 'blue');
+test.set('kite', 'pink');
+test.set('lion', 'golden');
+
+console.log(JSON.stringify(test));
+
+test.set('hat', 'yellow');
+console.log(JSON.stringify(test));
